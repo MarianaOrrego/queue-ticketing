948c847c0385460be36b8a4ca1dffef650441cd1
Mariana Orrego <83648336+MarianaOrrego@users.noreply.github.com>
2024-03-18 09:17:42 -0500

    Add objectManager to index.ts

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,3 +1,4 @@
 export { ClientType, Client, QueueManager } from "./client";
 export { Agent, AgentManager } from "./agent";
 export { DisplayManager } from "./display";
+export { objectManager } from "./objectManager";
