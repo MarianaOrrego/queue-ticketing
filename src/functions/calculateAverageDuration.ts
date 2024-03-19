3cb0b7b0c2fc17813ee52f7a76bdde280dfdfc0a
Mariana Orrego <83648336+MarianaOrrego@users.noreply.github.com>
2024-03-19 09:08:44 -0500

    Add calculateAverageDuration function and update AverageTable component

---
+++ src/functions/calculateAverageDuration.ts
@@ -0,0 +1,20 @@
+import { Client } from "../classes";
+
+export const calculateAverageDuration = (users: Client[]) => {
+  if (users.length === 0) return "00:00:00";
+
+  const totalDuration = users.reduce((acc, client) => {
+    const time = client.duration.split(":").map(Number);
+    const seconds = time[0] * 3600 + time[1] * 60 + time[2];
+    return acc + seconds;
+  }, 0);
+
+  const averageSeconds = totalDuration / users.length;
+  const hours = Math.floor(averageSeconds / 3600);
+  const minutes = Math.floor((averageSeconds % 3600) / 60);
+  const seconds = Math.floor(averageSeconds % 60);
+
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+};
